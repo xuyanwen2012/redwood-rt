bedfa62f62cf507821f97b522197f59dddef0d5f
xuyanwen2012 <xuyanwen2012@gmail.com>
2023-02-15 17:34:06 -0800

    add dummy kernel

--- accelerator/sycl/Redwood.cpp
+++ accelerator/sycl/Redwood.cpp
@@ -1,6 +1,8 @@
 #include <CL/sycl.hpp>
+#include <cmath>
 #include <iomanip>
 #include <iostream>
+#include <limits>
 
 #include "Redwood/Point.hpp"
 #include "SyclUtils.hpp"
@@ -83,13 +85,55 @@ void UsmFree(void* ptr) {
   }
 }
 
+inline auto rsqrtf(const float x) { return 1.0f / sqrtf(x); }
+
+// Modified version
+inline float KernelFuncBh(const Point4F p, const Point4F q) {
+  const auto dx = p.data[0] - q.data[0];
+  const auto dy = p.data[1] - q.data[1];
+  const auto dz = p.data[2] - q.data[2];
+  const auto dist_sqr = dx * dx + dy * dy + dz * dz + 1e-9f;
+  const auto inv_dist = rsqrtf(dist_sqr);
+  const auto inv_dist3 = inv_dist * inv_dist * inv_dist;
+  const auto with_mass = inv_dist3 * p.data[3];
+  return dx * with_mass + dy * with_mass + dz * with_mass;
+}
+
+inline float KernelFuncKnn(const Point4F p, const Point4F q) {
+  auto dist = float();
+
+  for (int i = 0; i < 4; ++i) {
+    const auto diff = p.data[i] - q.data[i];
+    dist += diff * diff;
+  }
+
+  return sqrtf(dist);
+}
+
 void ComputeOneBatchAsync(const int* u_leaf_indices,  /**/
                           const int num_active_leafs, /**/
                           float* out,                 /**/
                           const Point4F* u_lnt_data,  /**/
                           const int* u_lnt_sizes,     /**/
                           const Point4F q,            /**/
-                          const int stream_id) {}
+                          const int stream_id) {
+  const auto leaf_max_size = 64;
+  qs[stream_id].submit([&](sycl::handler& h) {
+    h.parallel_for(sycl::range(num_active_leafs), [=](const sycl::id<1> idx) {
+      const auto leaf_id = u_leaf_indices[idx];
+
+      auto my_sum = 0.0f;
+      for (int i = 0; i < leaf_max_size; ++i) {
+        const auto dist =
+            KernelFuncKnn(u_lnt_data[leaf_id * leaf_max_size + i], q);
+        my_sum += dist;
+      }
+
+      // Results will be pointing to a USM unique to each executor.
+      out[0] = my_sum;
+    });
+  });
+}
 
 void ProcessKnnAsync(const int* u_leaf_indices,  /**/
                      const Point4F* u_q_points,  /**/
@@ -97,6 +141,26 @@ void ProcessKnnAsync(const int* u_leaf_indices,  /**/
                      float* out,                 /**/
                      const Point4F* u_lnt_data,  /**/
                      const int* u_lnt_sizes,     /**/
-                     const int stream_id) {}
+                     const int stream_id) {
+  const auto leaf_max_size = 64;
+  qs[stream_id].submit([&](sycl::handler& h) {
+    h.parallel_for(sycl::range(num_active_leafs), [=](const sycl::id<1> idx) {
+      // idx = tid = index in the buffer
+      const auto leaf_id = u_leaf_indices[idx];
+      const auto q_point = u_q_points[idx];
+
+      auto my_min = std::numeric_limits<float>::max();
+      for (int i = 0; i < leaf_max_size; ++i) {
+        const auto dist =
+            KernelFuncBh(u_lnt_data[leaf_id * leaf_max_size + i], q_point);
+
+        my_min = sycl::min(my_min, dist);
+      }
+
+      // Results will be pointing to a USM unique to each executor.
+      out[0] = sycl::min(out[0], my_min);
+    });
+  });
+}
 
 }  // namespace redwood
