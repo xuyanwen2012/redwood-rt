380837f69640a310dbe5ee2b713f136273abf031
Zheyuan <sephirotheca17@gmail.com>
2023-05-01 17:40:14 -0700

    nn pb

---
+++ examples/nn_pb/Main.cpp
@@ -0,0 +1,191 @@
+#include <omp.h>
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <vector>
+
+#include "../LoadFile.hpp"
+#include "../Utils.hpp"
+#include "../cxxopts.hpp"
+#include "AppParams.hpp"
+#include "Executor.hpp"
+#include "Functors/DistanceMetrics.hpp"
+#include "GlobalVars.hpp"
+#include "KDTree.hpp"
+#include "ReducerHandler.hpp"
+#include "Redwood.hpp"
+
+_NODISCARD inline Point4F RandPoint() {
+  Point4F p;
+  p.data[0] = MyRand(0, 1024);
+  p.data[1] = MyRand(0, 1024);
+  p.data[2] = MyRand(0, 1024);
+  p.data[3] = MyRand(0, 1024);
+  return p;
+}
+
+int main(int argc, char** argv) {
+  cxxopts::Options options("Nearest Neighbor (NN)",
+                           "Redwood NN demo implementation");
+
+  // clang-format off
+  options.add_options()
+    ("f,file", "Input file name", cxxopts::value<std::string>())
+    ("m,query", "Number of particles to query", cxxopts::value<int>()->default_value("1048576"))
+    ("t,thread", "Number of threads", cxxopts::value<int>()->default_value("1"))
+    ("l,leaf", "Maximum leaf node size", cxxopts::value<int>()->default_value("32"))
+    ("b,batch_size", "Batch size (GPU)", cxxopts::value<int>()->default_value("1024"))
+    ("c,cpu", "Enable CPU baseline", cxxopts::value<bool>()->default_value("false"))
+    ("h,help", "Print usage");
+  // clang-format on
+
+  options.parse_positional({"file", "query"});
+
+  const auto result = options.parse(argc, argv);
+
+  if (result.count("help")) {
+    std::cout << options.help() << std::endl;
+    exit(EXIT_SUCCESS);
+  }
+
+  if (!result.count("file")) {
+    std::cerr
+        << "requires an input file (\"../../data/1m_nn_uniform_4f.dat\")\n";
+    std::cout << options.help() << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  const auto data_file = result["file"].as<std::string>();
+  app_params.m = result["query"].as<int>();
+  app_params.num_threads = result["thread"].as<int>();
+  app_params.max_leaf_size = result["leaf"].as<int>();
+  app_params.batch_size = result["batch_size"].as<int>();
+  app_params.cpu = result["cpu"].as<bool>();
+  std::cout << app_params << std::endl;
+
+  std::cout << "Loading Data..." << std::endl;
+
+  const auto in_data = load_data_from_file<Point4F>(data_file);
+  const auto n = in_data.size();
+
+  // For each thread
+  std::vector<std::queue<Task>> q_data(app_params.num_threads);
+  const auto tasks_per_thread = app_params.m / app_params.num_threads;
+  for (int tid = 0; tid < app_params.num_threads; ++tid) {
+    for (int i = 0; i < tasks_per_thread; ++i) {
+      q_data[tid].emplace(i, RandPoint());
+    }
+  }
+
+  std::cout << "Building kd Tree..." << std::endl;
+
+  const kdt::KdtParams params{app_params.max_leaf_size};
+  tree_ref = std::make_shared<kdt::KdTree>(params, in_data.data(), n);
+
+  const auto num_leaf_nodes = tree_ref->GetStats().num_leaf_nodes;
+  auto lnt_addr = rdc::AllocateLnt(num_leaf_nodes, app_params.max_leaf_size);
+  tree_ref->LoadPayload(lnt_addr);
+
+  // Init
+  rdc::Init(app_params.num_threads, app_params.batch_size);
+  omp_set_num_threads(app_params.num_threads);
+  final_results1.resize(app_params.m);
+
+  std::cout << "Starting Traversal..." << std::endl;
+  if (app_params.cpu) {
+    TimeTask("CPU Traversal", [&] {
+      std::vector<Executor<dist::Euclidean>> cpu_exe;
+      for (int tid = 0; tid < app_params.num_threads; ++tid) {
+        cpu_exe.emplace_back(tid, 0, 0);
+      }
+
+      // Run
+#pragma omp parallel for
+      for (int tid = 0; tid < app_params.num_threads; ++tid) {
+        while (!q_data[tid].empty()) {
+          const auto task = q_data[tid].front();
+          cpu_exe[tid].SetQuery(task);
+          final_results1[task.first] = cpu_exe[tid].CpuTraverse();
+          q_data[tid].pop();
+        }
+      }
+    });
+
+  } else {
+    // Use Redwood
+    constexpr auto num_streams = 2;
+
+    // Setup traversers
+    std::vector<Executor<dist::Euclidean>> exes;
+    exes.reserve(app_params.num_threads * num_streams * app_params.batch_size);
+    for (int tid = 0; tid < app_params.num_threads; ++tid) {
+      for (int stream_id = 0; stream_id < num_streams; ++stream_id) {
+        for (int i = 0; i < app_params.batch_size; ++i) {
+          exes.emplace_back(tid, stream_id, i);
+        }
+      }
+    }
+
+    // exe[4][2][1024]
+    const auto tid_offset = num_streams * app_params.batch_size;
+    const auto stream_offset = app_params.batch_size;
+
+    TimeTask("GPU Traversal...", [&] {
+#pragma omp parallel for
+      for (int tid = 0; tid < app_params.num_threads; ++tid) {
+        auto cur_stream = 0;
+        while (!q_data[tid].empty()) {
+          auto it = tid * tid_offset + cur_stream * stream_offset;
+          const auto it_end = it + app_params.batch_size;
+          for (; it != it_end;) {
+            if (exes[it].Finished()) {
+              if (!q_data[tid].empty()) {
+                const auto q = q_data[tid].front();
+                q_data[tid].pop();
+
+                exes[it].SetQuery(q);
+                exes[it].StartQuery();
+              }
+
+              ++it;
+            } else {
+              exes[it].Resume();
+              if (exes[it].Finished()) {
+                // Do not increment 'it'
+              } else {
+                ++it;
+              }
+            }
+          }
+
+          rdc::LaunchAsyncWorkQueue(tid, cur_stream);
+
+          // switch to next
+          cur_stream = (cur_stream + 1) % num_streams;
+
+          redwood::DeviceStreamSynchronize(tid, cur_stream);
+          rdc::ResetBuffer(tid, cur_stream);
+        }
+      }
+
+      redwood::DeviceSynchronize();
+
+      const auto left =
+          app_params.num_threads * num_streams * app_params.batch_size;
+      for (int i = 0; i < left; ++i) {
+        const auto q_idx = exes[i].my_task_.first;
+        final_results1[q_idx] = exes[i].CpuTraverse();
+      }
+    });
+  }
+  std::cout << "Program Execution Completed. " << std::endl;
+
+  rdc::Release();
+  return EXIT_SUCCESS;
+}
---
+++ examples/nn_pb/ReducerHandler.hpp
@@ -0,0 +1,154 @@
+#pragma once
+
+#include <array>
+
+#include "../Utils.hpp"
+#include "Functors/DistanceMetrics.hpp"
+#include "KnnSet.hpp"
+#include "Redwood.hpp"
+
+using Task = std::pair<int, Point4F>;
+
+namespace rdc {
+
+// Shared accross threads, streams.
+inline Point4F* lnt_base_addr = nullptr;
+inline int stored_max_leaf_size;
+
+_NODISCARD inline Point4F* AllocateLnt(const int num_leaf_nodes,
+                                       const int max_leaf_size) {
+  stored_max_leaf_size = max_leaf_size;
+  lnt_base_addr = redwood::UsmMalloc<Point4F>(num_leaf_nodes * max_leaf_size);
+  return lnt_base_addr;
+}
+
+_NODISCARD inline const Point4F* LntDataAddrAt(const int node_idx) {
+  return lnt_base_addr + node_idx * stored_max_leaf_size;
+}
+
+// For NN and KNN
+struct Buffer {
+  void Alloc(const int buffer_size) {
+    u_qs = redwood::UsmMalloc<Point4F>(buffer_size);
+    u_leaf_idx = redwood::UsmMalloc<int>(buffer_size);
+  }
+
+  void DeAlloc() const {
+    redwood::UsmFree(u_qs);
+    redwood::UsmFree(u_leaf_idx);
+  }
+
+  _NODISCARD int Size() const { return num_active; }
+
+  void Reset() { num_active = 0; }
+
+  void Push(const Task& task, const int node_idx) {
+    u_qs[num_active] = task.second;
+    u_leaf_idx[num_active] = node_idx;
+    ++num_active;
+  }
+
+  int num_active;
+  Point4F* u_qs;
+  int* u_leaf_idx;
+};
+
+struct ResultBuffer {
+  void Alloc(const int buffer_size, const int k = 1) {
+    stored_k = k;
+    underlying_dat = redwood::UsmMalloc<float>(buffer_size * k);
+  }
+
+  void DeAlloc() const { redwood::UsmFree(underlying_dat); }
+
+  _NODISCARD float* GetAddrAt(const int executor_id) const {
+    return underlying_dat + executor_id * stored_k;
+  }
+
+  float* underlying_dat;
+  int stored_k;
+};
+
+inline int stored_num_threads;
+inline std::vector<std::array<Buffer, 2>> buffers;
+inline std::vector<std::array<ResultBuffer, 2>> result_addr;
+
+inline void Init(const int num_thread, const int batch_size) {
+  redwood::Init(num_thread);
+  stored_num_threads = num_thread;
+
+  buffers.resize(num_thread);
+  result_addr.resize(num_thread);
+  for (int tid = 0; tid < num_thread; ++tid) {
+    for (int i = 0; i < 2; ++i) {
+      buffers[tid][i].Alloc(batch_size);
+      result_addr[tid][i].Alloc(batch_size);
+
+      redwood::AttachStreamMem(tid, i, buffers[tid][i].u_leaf_idx);
+      redwood::AttachStreamMem(tid, i, buffers[tid][i].u_qs);
+      redwood::AttachStreamMem(tid, i, result_addr[tid][i].underlying_dat);
+    }
+  }
+}
+
+inline void Release() {
+  for (int tid = 0; tid < stored_num_threads; ++tid) {
+    for (int i = 0; i < 2; ++i) {
+      buffers[tid][i].DeAlloc();
+      result_addr[tid][i].DeAlloc();
+    }
+  }
+
+  redwood::UsmFree(lnt_base_addr);
+}
+
+inline void ResetBuffer(const int tid, const int cur_stream) {
+  buffers[tid][cur_stream].Reset();
+}
+
+_NODISCARD inline float* RequestResultAddr(const int tid, const int stream_id,
+                                           const int executor_index) {
+  return result_addr[tid][stream_id].GetAddrAt(executor_index);
+}
+
+inline void ReduceLeafNode(const int tid, const int stream_id, const Task& task,
+                           const int node_idx) {
+  buffers[tid][stream_id].Push(task, node_idx);
+}
+
+inline void DebugCpuReduction(const Buffer& buf, const dist::Euclidean functor,
+                              const ResultBuffer& results) {
+  const auto n = buf.Size();
+
+  // i is batch id, = tid, = index in the buffer
+  for (int i = 0; i < n; ++i) {
+    const auto node_idx = buf.u_leaf_idx[i];
+    const auto q = buf.u_qs[i];
+
+    const auto node_addr = LntDataAddrAt(node_idx);
+    const auto addr = reinterpret_cast<KnnSet<float, 1>*>(results.GetAddrAt(i));
+
+    for (int j = 0; j < stored_max_leaf_size; ++j) {
+      const float dist = functor(node_addr[j], q);
+      addr->Insert(dist);
+    }
+  }
+}
+
+inline void LaunchAsyncWorkQueue(const int tid, const int stream_id) {
+  const auto num_active = buffers[tid][stream_id].Size();
+
+  if constexpr (kDebugMod) {
+    std::cout << "rdc::LaunchAsyncWorkQueue "
+              << "tid: " << tid << ", stream: " << stream_id << ", "
+              << buffers[tid][stream_id].Size() << " actives." << std::endl;
+    // 128? 256?
+  }
+
+  dist::Euclidean functor{};
+  redwood::NearestNeighborKernel(
+      tid, stream_id, lnt_base_addr, stored_max_leaf_size,
+      buffers[tid][stream_id].u_qs, buffers[tid][stream_id].u_leaf_idx,
+      num_active, result_addr[tid][stream_id].underlying_dat, functor);
+}
+}  // namespace rdc
